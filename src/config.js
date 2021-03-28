1c1653412b1fd99ddab5c0f681ecb2ce2fd5657f
souleymane kone <desolo74@gmail.com>
2021-03-28 04:42:54 -0700

    heroku

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8001,
     NODE_ENV: process.env.NODE_ENV || 'production',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
     API_TOKEN: process.env.API_TOKEN || 'a15f9448-d074-4cdc-a932-cbb5355fd7c2',
     DB_URL: process.env.DB_URL || 'postgresql://dunder_mifflin:hello@localhost/bookmarks',
   }
